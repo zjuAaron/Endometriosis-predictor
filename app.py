f8ac80143cda0e8b8e1abe7eddad1300e3146721
zjuAaron <sduaron@gmail.com>
2024-11-17 16:31:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ model = xgb.XGBClassifier()
 model.load_model('model.json')
 
 # Initialize SHAP explainer
-explainer = shap.Explainer(model)
+explainer = shap.TreeExplainer(model)
 
 # Define mappings
 race_mapping = {
